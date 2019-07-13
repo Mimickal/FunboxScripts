291f4f4d220688837b64c3adebf2efa8846532d8
Mimickal <mimickal.dev@gmail.com>
2019-07-12 21:18:32 -0400

    Add a git ignore to clean up git output

--- fuzzies.sh
+++ fuzzies.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# This file contains a bunch of bash functions that
-# improve existing utilities with fzf (fuzzy find)
+# This file contains a bunch of bash functions that use fzf (fuzzy find) to
+# create more interactive UIs for existing utilities.
 
+# cd into directory the selected file lives in
 function fcd() {
 	file="$(locate --nofollow /* | fzf)"
 	path=$(dirname "$file")
 	cd "$path"
 }
 
+# Kills selected process by ID
 function fkill() {
 	kill $(ps fux | fzf | sed 's/[a-zA-Z]* *\([0-9]*\).*/\1/')
 }
