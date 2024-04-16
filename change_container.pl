40ee29d1b903beb704baad9557049a480efab92c
Mia Moretti <mimickal.dev@gmail.com>
2024-04-16 02:15:11 -0400

    Overhaul change_container.pl
    - Use POD instead of awful string help text.
    - Reorganize and standardize command line options without breaking too many things.
    - Add support for forwarding h264 arguments to ffmpeg.
    - Respect log level a little more.

--- change_container.pl
+++ change_container.pl
@@ -1,14 +1,18 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
-no warnings 'uninitialized';
 
+use feature 'say';
+
+use Data::Dumper qw( Dumper );
+use English;
 use File::Basename qw( fileparse );
 use File::Glob qw( bsd_glob );
 use Getopt::Long;
 use IPC::Run3 qw( run3 );
+use Pod::Usage qw( pod2usage );
 
-our $VERSION = '2.6';
+our $VERSION = '2.7';
 
 # TODO actually implement log levels for ffmpeg subprocesses
 # TODO actually implement progress
@@ -18,88 +22,94 @@ our $VERSION = '2.6';
 # name with "file:"
 # TODO Check return code from run3 so if ffmpeg fails, we don't exit
 # successfully
-# TODO instead of appending .conv, move the old file into an ./old/ directory
 # TODO embed srt files into converted media
-
-
-use constant INFO => qq{
-Converts a media files to the Funbox Watch format.
-    Container  mp4
-    Encoding   h.264
-    Audio      aac
-    Subtitles  mov_text (if text, copy otherwise)
-
-Videos already encoded with h.264 (typically .mkv files) will simply have
-their video container changed without re-encoding the video.
-Multiple audio tracks will be preserved (but Watch will only play the first).
-Subtitle tracks will be converted to mov_text if they are text-based, and
-preserved if they are image-based. Attachments will be preserved. The output
-file has .conv appended to the name if it would otherwise overwrite the input
-file.
-};
-
-use constant OPTIONS => qq{
-Options:
-  -h --help       Outputs this help text.
-  -m --mock       Output operations without actually running the conversion.
-  -o --out-dir    Specifies a separate output directory. Defaults to the same
-                  directory as the source file.
-  -s --scale      Scale the output resolution. For example, if "720p" is given
-                  for a 1920x1080 video, the script will set the output height
-                  to 720 and auto-determine the height to maintain aspect ratio.
-                  You should verify the input file's resolution with ffprobe
-                  before using this to avoid fucking things up.
-  -l --log-level  Sets the output level. Valid options:
-                      none      Output nothing at all
-                      info      Output operations only (useful for cron jobs)
-                      progress  (Default) Output operations with progress bar
-                      debug     Output full info from ffmpeg
-     --ignore     Ignore the given tracks. Format: <id1>[,<id2>,<id3>...]
-     --override   Override tracks instead of using defaults.
-                  Format <track id>=<format>,[<track id>=<format>].
-};
 
 use constant LOG_LEVELS => {
 	none => 0,
-	info => 1,
-	progress => 2,
-	debug => 3,
+	warn => 1,
+	info => 2,
+	progress => 3,
+	debug => 4,
 };
 
 my %Args;
 GetOptions(
-	'ignore:s'      => \($Args{ignore}),
-	'override:s'    => \($Args{override}),
-	'l|log-level:s' => \($Args{log_level} = 'progress'),
+	'a|h264-args:s' => \($Args{h264_args}),
+	'e|override:s'  => \($Args{override}),
+	'i|ignore:s'    => \($Args{ignore}),
+	'l|log-level:s' => \($Args{log_level} = 'info'),
 	'm|mock'        => \($Args{mock}),
 	'o|out-dir:s'   => \($Args{out_dir}),
 	's|scale:s'     => \($Args{scale}),
-	'v|version'     => sub { print("Version $VERSION\n"); exit(0); },
-	'h|help'        => sub { Usage(); },
-) or die Usage($!);
+	'v|version'     => sub { say("Version $VERSION"); exit(0); },
+	'h|help'        => sub {
+		pod2usage({
+			-exitval => 0,
+			-verbose => 99,
+			-sections => [qw( DESCRIPTION SYNOPSIS OPTIONS)],
+		});
+	},
+) or pod2usage({ -exitval => $ERRNO });
 
-my $LogLevel = LOG_LEVELS->{$Args{log_level}};
+my $logLevel = LOG_LEVELS->{$Args{log_level}};
 
-unless (defined $LogLevel) {
-	Usage("Invalid log level [$Args{log_level}]");
+unless (defined($logLevel)) {
+	Error("Invalid log level [$Args{log_level}]");
 }
 
-my @ScaleArgs;
+if ($logLevel >= LOG_LEVELS->{debug}) {
+	Info("Args " . Dumper(\%Args));
+}
+
+if ($Args{out_dir} && !-d $Args{out_dir}) {
+	Error("Invalid output directory [$Args{out_dir}]");
+}
+
+my @ffmpegArgs;
+if ($logLevel < LOG_LEVELS->{debug}) {
+	push(@ffmpegArgs, '-hide_banner');
+}
+
+if ($logLevel == LOG_LEVELS->{info}) {
+	push(@ffmpegArgs, '-loglevel', 'info');
+} elsif ($logLevel == LOG_LEVELS->{warn}) {
+	push(@ffmpegArgs, '-loglevel', 'warning');
+} elsif ($logLevel == LOG_LEVELS->{none}) {
+	push(@ffmpegArgs, '-loglevel', 'fatal');
+}
+
+my @videoArgs;
 if ($Args{scale}) {
 	my ($height) = ($Args{scale} =~ /(\d+)[pP]/);
 	unless (defined($height)) {
-		Log("Invalid scale value [$Args{scale}]");
-		exit(1);
+		Error("Invalid scale value [$Args{scale}]");
 	}
+
 	# Using -1 makes ffmpeg calculate width based on height and aspect ratio
-	@ScaleArgs = ('-filter:v', qq(scale=-1:$height));
+	@videoArgs = ('-filter:v', qq(scale=-1:$height));
+}
+if (defined($Args{h264_args})) {
+	push (@videoArgs, split(/\s+/, $Args{h264_args}));
 }
 
+my @ignoreTracks =
+	map {('-map', "-0:$_")}
+	split(/,/, $Args{ignore} // '');
+
+# Telling ffmpeg to ignore a track shifts the track numbers in the output file.
+# Subtract ignore arguments from the output stream numbers so callers don't need
+# to care about keeping track of that shift (i.e. can use IDs from ffprobe).
+my @overrideTracks = map {
+	my ($stream, $format) = split(/=/, $_);
+	$stream -= (scalar(@ignoreTracks) / 2);
+	("-c:$stream", $format);
+} split(/,/, $Args{override} // '');
+
+# Do all the conversions, globbing directories if necessary.
 for my $path (@ARGV) {
 	if (-f $path) {
 		ConvertFile($path);
-	}
-	elsif (-d $path) {
+	} elsif (-d $path) {
 		for my $path (bsd_glob("$path/*")) {
 			if (!-d $path) {
 				ConvertFile($path);
@@ -108,28 +118,19 @@ for my $path (@ARGV) {
 	}
 }
 
-sub Log {
-	my ($msg) = @_;
-	if ($LogLevel >= LOG_LEVELS->{info}) {
-		print("$msg\n");
-	}
-}
-
 sub ConvertFile {
 	my ($path) = @_;
-
-	my ($basename, $dir, $suffix) = fileparse($path, qr/\.[^.]*/);
 
 	my $vformat = GetCodec($path, 'v:0');
 	my $aformat = GetCodec($path, 'a:0');
 	my $sformat = GetCodec($path, 's:0');
 
 	unless ($vformat) {
-		Log("$path - Skipping, not a media file");
+		Info("Skipping (not a media file): $path");
 		return;
 	}
 
-	my $vidcodec = ($vformat eq 'h264' && ! @ScaleArgs) ? 'copy' : 'libx264';
+	my $vidcodec = ($vformat eq 'h264' && !@videoArgs) ? 'copy' : 'libx264';
 	my $audcodec = ($aformat eq 'aac') ? 'copy' : 'aac';
 	my $subcodec = (
 		# Manually determined from https://stackoverflow.com/a/64500869/7954860
@@ -141,15 +142,7 @@ sub ConvertFile {
 		)
 	) ? 'copy' : 'mov_text';
 
-	my @ignores = map {('-map', "-0:$_")} split(/,/, $Args{ignore});
-
-	# Ignoring changes the output track numbers, so subtract ignore arguments
-	# from the stream numbers so callers don't need to care about that math.
-	my @overrides = map {
-		my ($stream, $format) = split(/=/, $_);
-		$stream -= (scalar(@ignores) / 2);
-		("-c:$stream", $format);
-	} split(/,/, $Args{override});
+	my ($basename, $dir, $suffix) = fileparse($path, qr/\.[^.]*/);
 
 	if (
 		$vidcodec eq 'copy' &&
@@ -157,38 +150,36 @@ sub ConvertFile {
 		$subcodec eq 'copy' &&
 		$suffix eq '.mp4'
 	) {
-		Log("$path - Skipping, already in proper format");
+		Info("Skipping (already in proper format): $path");
 		return;
 	}
 
-	Log("Converting: $path");
+	Info("Converting: $path");
 
-	my $input = "$dir/$basename$suffix";
-	my $output = $Args{out_dir} // $dir;
-	$output .= "/$basename.mp4";
+	my $output = "@{[ $Args{out_dir} // $dir ]}/$basename.mp4";
 
-	if ($input eq $output) {
-		Log("Output path matches input path. Appending .conv to output name");
+	if ($path eq $output) {
+		Info("Output path matches input path. Appending .conv to output name");
 		$output .= ".conv";
 	}
 
 	my $ffmpeg_params = [
 		'ffmpeg',
-		'-i', $input,
+		@ffmpegArgs,
+		'-i', $path,
 		'-c:v', $vidcodec,
 		'-map', '0',
-		@ignores,
-		@ScaleArgs,
+		@ignoreTracks,
+		@videoArgs,
 		'-c:a', $audcodec,
 		'-c:s', $subcodec,
 		'-c:t', 'copy',
-		@overrides,
+		@overrideTracks,
 		'-f', 'mp4', $output,
 	];
 
 	if ($Args{mock}) {
-		use Data::Dumper;
-		print Dumper $ffmpeg_params;
+		Info(Dumper($ffmpeg_params));
 	} else {
 		run3($ffmpeg_params);
 	}
@@ -213,20 +204,123 @@ sub GetCodec {
 	return $out;
 }
 
-sub Usage {
+sub Error {
 	my ($msg) = @_;
-	my ($scriptname) = fileparse($0);
-
-	my $output = qq{$msg
-Usage:
-    $scriptname path/to/media
-};
-
-	$output .= OPTIONS;
-	$output .= INFO;
-
-	print ($output);
-
-	# Exit with status code 1 if $msg is provided. Assume this means error.
-	exit(!!$msg);
+	say(STDERR "Error: $msg");
+	exit(1);
 }
+
+sub Info {
+	my ($msg) = @_;
+	if ($logLevel >= LOG_LEVELS->{info}) {
+		say($msg);
+	}
+}
+
+=pod
+
+=head1 NAME
+
+change_container - Converts a video file to a widely supported format.
+
+=head1 SYNOPSIS
+
+change_container <media_file_1> [media_file_2] ...
+
+=head1 DESCRIPTION
+
+Takes any video file and converts it to the most widely supported format we can.
+We prioritize compatibility across browsers and devices first, quality second,
+and file size third.
+
+=head2 Format
+
+=over
+
+=item S<Container    MP4>
+
+=item S<Video        H.264>
+
+=item S<Audio        AAC>
+
+=item S<Subtitles    mov_text (if text, "copy" if otherwise, e.g. image-based)>
+
+=item S<Attachments  copy>
+
+=back
+
+=head2 Notes
+
+=over
+
+=item - If a given path is a directory instead of a file, all media files within
+that directory will be converted.
+
+=item - This script avoids re-encoding when possible
+(e.g. all correct encoding, just in an MKV instead of MP4).
+
+=item - Multiple audio tracks are preserved
+(but web players will only play the first track).
+
+=item - The output file has .conv appended to the name if it would otherwise
+overwrite the input file.
+
+=back
+
+=head1 OPTIONS
+
+=over
+
+=item B<-a --h264-args>
+
+Additional H.264 arguments. Remember to use quotes, if necessary.
+
+Example: C<--h264-args="-crf 28 -tune grain -preset slow">
+
+See L<https://trac.ffmpeg.org/wiki/Encode/H.264>
+
+=item B<-e --override>S<  Override track encoding instead of using defaults.>
+
+Format: <track_id_1>=<format_1>[,<track_id_2>=<format_2>,...]
+
+Example: 1=h264,4=copy
+
+=item B<-i --ignore>S<    Don't include the given tracks in the output file.>
+
+Format: <id1>[,<id2>,<id3>...]
+
+=item B<-l --log-level>S<  Sets the log output level.>
+
+    none      Output only show-stopping errors.
+    warn      Output actions, warnings, and errors (useful for cron jobs).
+    info      (Default) Output all the ffmpeg track info.
+    progress  Output operations with progress bar (currently does nothing).
+    debug     Output full info from ffmpeg.
+
+=item B<-m --mock>
+
+Output operations without actually running the conversion.
+
+=item B<-o --out-dir>
+
+Specifies an output directory. Defaults to same directory as the source file.
+
+=item B<-s --scale>
+
+Scale the output resolution. For example, if "720p" is given for a 1920x1080
+video, the script will set the output height to 720 and auto-determine the width
+to maintain aspect ratio. Verify the input file's resolution with L<ffprobe>
+before using this to avoid fucking things up.
+
+=item B<-h --help>S<     Output this help text and exit.>
+
+=item B<-v --version>S<  Output version and exit.>
+
+=back
+
+=head1 LICENSE
+
+GPL-3.0
+
+=cut
+
