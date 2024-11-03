fcc4b1948646bc1ce09f87596c4e565dcaf64b6b
Mia Moretti <mimickal.dev@gmail.com>
2024-11-02 23:15:07 -0400

    Don't explode if we can't fetch IP info

--- print-ip-info.py
+++ print-ip-info.py
@@ -2,14 +2,17 @@
 import json
 from urllib.request import urlopen
 
-VERSION = '1.0'
+VERSION = '1.1'
 ENDPOINT = 'https://wtfismyip.com/json'
 
-info_json = urlopen('https://wtfismyip.com/json').read()
-info = json.loads(info_json)
+try:
+	info_json = urlopen('https://wtfismyip.com/json').read()
+	info = json.loads(info_json)
 
-location = info['YourFuckingLocation']
-ip = info['YourFuckingIPAddress']
+	location = info['YourFuckingLocation']
+	ip = info['YourFuckingIPAddress']
 
-print(f'{ip} ({location})')
+	print(f'{ip} ({location})')
+except:
+	print('Failed to fetch IP info. Your network (including VPN) may be disconnected.')
 
