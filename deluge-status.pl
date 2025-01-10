6233d09eb74d7dfccc96d8cd1dd04ff30453b91b
Mia Moretti <mimickal.dev@gmail.com>
2025-01-09 22:46:13 -0500

    Add version to deluge-status script

--- deluge-status.pl
+++ deluge-status.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use feature 'say';
 
+our $VERSION = '1.0';
+
 my $running = grep { $_ =~ /deluged/ } split(/\n/, `ps x`);
 say('Deluge daemon is ' . ($running ? "\e[32m" : "\e[33mNOT ") . "running\e[0m");
 
